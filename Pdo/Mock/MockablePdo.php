5e0744fabb49aed48805ff41054315f9a167c7f6
Reynaldo Castellano III <reynaldocastellano@gmail.com>
2016-06-20 11:48:07 +0800

    Make MockablePdo extend from PDO

--- Pdo/Mock/MockablePdo.php
+++ Pdo/Mock/MockablePdo.php
@@ -2,73 +2,8 @@
 
 namespace Naldz\Bundle\TestUtilityBundle\Pdo\Mock;
 
-class MockablePdo
+class MockablePdo extends \PDO
 {
     public function __construct(){
-
-    }
-
-    public function beginTransaction()
-    {
-    }
-
-    public function commit()
-    {
-    }
-
-    public function errorCode()
-    {
-    }
-
-    public function errorInfo()
-    {
-    }
-
-    public function exec()
-    {
-    }
-
-    public function getAttribute()
-    {
-    }
-
-    public function getAvailableDrivers()
-    {
-    }
-
-    public function inTransaction()
-    {
-    }
-
-    public function prepare()
-    {
-    }
-
-    public function query()
-    {
-    }
-
-    public function quote()
-    {
-    }
-
-    public function rollBack()
-    {
-    }
-
-    public function setAttribute()
-    {
-    }
-
-    public function __sleep()
-    {
-    }
-
-    public function __wakeup()
-    {
-    }
-
-    public function lastInsertId()
-    {
     }
 }
